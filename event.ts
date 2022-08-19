d20b1710bbe2a51d5cb0036fa441fc7572b66042
xharris <xhh950@gmail.com>
2022-08-19 17:40:31 -0400

    adds socket cbs to event/plan

--- event.ts
+++ event.ts
@@ -1,6 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { Eventful } from 'types'
-import { api } from './api'
+import { api, useSocket } from './api'
 
 export const useEvents = () => {
   const query = useQuery<Eventful.API.EventGet[]>(['events'], () =>
@@ -31,6 +31,52 @@ export const useEvent = ({ id }: { id?: Eventful.ID | string }) => {
     { enabled: !!id }
   )
   const qc = useQueryClient()
+  const { useOn } = useSocket()
+
+  useOn(
+    'plan:add',
+    (plan: Eventful.API.PlanGet) => {
+      qc.setQueriesData<Eventful.API.EventGet>(['event', { id }], (old) =>
+        old
+          ? {
+              ...old,
+              plans: [plan, ...old.plans.filter((pln) => pln._id !== plan._id)],
+            }
+          : undefined
+      )
+    },
+    [qc, id]
+  )
+
+  useOn(
+    'plan:edit',
+    (plan: Eventful.API.PlanGet) => {
+      qc.setQueriesData<Eventful.API.EventGet>(['event', { id }], (old) =>
+        old
+          ? {
+              ...old,
+              plans: [plan, ...old.plans.filter((pln) => pln._id !== plan._id)],
+            }
+          : undefined
+      )
+    },
+    [qc, id]
+  )
+
+  useOn(
+    'plan:delete',
+    (plan: Eventful.API.PlanGet) => {
+      qc.setQueriesData<Eventful.API.EventGet>(['event', { id }], (old) =>
+        old
+          ? {
+              ...old,
+              plans: old.plans.filter((pln) => pln._id !== plan._id),
+            }
+          : undefined
+      )
+    },
+    [qc, id]
+  )
 
   const muUpdateEvent = useMutation(
     (body: Eventful.API.EventUpdate) => api.put(`event/${id}`, body),
